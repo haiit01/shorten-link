7ee9be681e0f00f9efff72e31fc827d4f2c4cc0a
haiit01 <haiit01@icloud.com>
2024-11-17 04:01:56 +0000

    change cable

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2024_11_15_143434) do
+ActiveRecord::Schema[8.0].define(version: 2024_11_17_035827) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_catalog.plpgsql"
+
+  create_table "cable_tables", force: :cascade do |t|
+    t.binary "channel", null: false
+    t.binary "payload", null: false
+    t.datetime "created_at", null: false
+    t.bigint "channel_hash", null: false
+    t.index ["channel"], name: "index_solid_cable_messages_on_channel"
+    t.index ["channel_hash"], name: "index_solid_cable_messages_on_channel_hash"
+    t.index ["created_at"], name: "index_solid_cable_messages_on_created_at"
+  end
 
   create_table "links", force: :cascade do |t|
     t.string "original_url", null: false
